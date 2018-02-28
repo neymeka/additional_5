0d76be1ca928e6e7a571ceecb6aef2eda16bddea
neymeka <megustafather@gmail.com>
2018-02-28 22:01:13 +0300

    feacher/master: Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+if (str.length % 2 != 0) {
+  return false
+}
+var stack = [];
+for (var i = 0; i < str.length; i++) {
+for (var j = 0; j < bracketsConfig.length; j++) {
+if (str[i] == bracketsConfig[j][0]) {
+  stack.push(str[i]);
+  }
+if (str[i] == bracketsConfig[j][1]) {
+  stack.pop(str[i]);
+  }
+}
+}
+if (stack == 0) {return true}
+ else {return false}
 }
